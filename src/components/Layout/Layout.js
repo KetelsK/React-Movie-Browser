ed893df676c39a04e476727b07915f4ff6826712
la163024 <la163024@student.helha.be>
2020-09-23 14:50:11 +0200

    Revert "Added pagination"
    This reverts commit fd6a1c149e5d115bc01deae5ce71204856c6ff0d.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,18 @@
-import React, { Component } from "react";
-import { Container } from "reactstrap";
-import NavBar from "./NavBar";
-import "./Layout.css";
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+import NavBar from './NavBar';
+import './Layout.css';
 
 export class Layout extends Component {
   static displayName = Layout.name;
 
-  render() {
+  render () {
     return (
-      <div>
-        <NavBar />
-        <Container>{this.props.children}</Container>
+        <div>
+            <NavBar />
+            <Container>
+                {this.props.children}
+            </Container>
       </div>
     );
   }
